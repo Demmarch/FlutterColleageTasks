c0afda851a9f2c9ce7d7ac8355c26aa9e0af3793
Demmarch <yar310306d@gmail.com>
2025-11-07 11:33:59 +0300

    main window laba4

--- laba4_signinsignup/lib/main.dart
+++ laba4_signinsignup/lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
 
-void main() {
+const String supabaseUrl = 'https://diatfsydzbqpfdzwcgil.supabase.co';
+const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpYXRmc3lkemJxcGZkendjZ2lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyMTIxNzIsImV4cCI6MjA3Njc4ODE3Mn0.o5w70G_DuDtwR2MEaylJC68g-UTN5dzOJmVVmzVog8w';
+
+final supabase = Supabase.instance.client;
+
+void main() async {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
+
   runApp(const MainApp());
 }
 
@@ -9,10 +25,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: 
+    return MaterialApp(
+      title: 'Supabase Auth Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
       ),
+      initialRoute: '/register',
+      routes: {
+        //'/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        // '/home': (context) => const HomeScreen(), // Экран после успешного входа
+      },
     );
   }
 }
